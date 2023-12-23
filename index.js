09052436e2b581ca4f3a5d5bf90bf320e6ea3a7c
Jiahan Chen <3238468729@qq.com>
2023-12-23 16:42:40 +0000

    update the index.js to accommodate the api

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import usersRouter from './api/users';
 import './db';
 import defaultErrHandler from './errHandler';
 import moviesRouter from './api/movies';
+import actorsRouter from './api/actors';
+import reviewsRouter from './api/reviews';
 import authenticate from './authenticate';
 import './seedData';
 
@@ -17,6 +19,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
 app.use('/api/movies',  moviesRouter);
+app.use('/api/actors', actorsRouter);
+app.use('/api/reviews', reviewsRouter);
 app.use(defaultErrHandler);
 
 let server = app.listen(port, () => {
