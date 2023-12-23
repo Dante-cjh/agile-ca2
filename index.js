256687dba3e2d0ad5e1cd349fc5584677d224cc6
Jiahan Chen <3238468729@qq.com>
2023-12-23 15:06:45 +0000

    Changes due to API from WAD2 lab

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import './db';
 import defaultErrHandler from './errHandler';
 import moviesRouter from './api/movies';
 import authenticate from './authenticate';
+import './seedData';
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use('/api/users', usersRouter);
 app.use('/api/movies',authenticate,  moviesRouter);
 app.use(defaultErrHandler);
 
-app.listen(port, () => {
+let server = app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
+module.exports = server
